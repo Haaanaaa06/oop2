5ce3157475b14b97e810e7e9100155d6673a4169
hanahatem <hanahatem06@gmail.com>
2026-09-02 21:44:53 +0300

    Add files via upload

---
+++ indexers/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexers
+{
+    class MyCookieCollection
+    {
+        private Dictionary<string, string> cookies = new Dictionary<string, string>();
+        public string this[string name]
+        {
+            get
+            {
+                if (cookies.ContainsKey(name))
+                    return cookies[name];
+                return null;
+            }
+            set
+            {
+                cookies[name] = value;
+            }
+
+
+        }
+
+
+
+        public void PRintAllCookies()
+        {
+            foreach (var cookie in cookies)
+            {
+                Console.WriteLine($"{cookie.Key}: {cookie.Value}");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            MyCookieCollection cookies = new MyCookieCollection();
+            cookies["username"] = "hana";
+            cookies["theme"] = "dark";
+
+
+            Console.WriteLine($"Username: {cookies["username"]}");
+            Console.WriteLine($"Theme: {cookies["theme"]}");    
+            Console.WriteLine($"Language: {cookies["language"]??"not set"}");
+            cookies.PRintAllCookies();
+
+        }
+    }
+}
